17df692fb08358dbd6b352204681eed5b3df8cf9
VitaliiPolushkinDigis <91121101+VitaliiPolushkinDigis@users.noreply.github.com>
2023-09-02 15:15:35 +0300

    change api url

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect } from 'react';
 import LoginForm from '../../components/forms/LoginForm/LoginForm';
 import Page from '../../components/layouts/Page/Page';
 import { useAppDispatch } from '../../store/store';
+import { SocketContext } from '../../utils/context/SocketContext';
+import { MessageEventPayload } from '../../utils/types';
 import { useStyles } from './LoginPage.helper';
 
 interface LoginPageProps {}
@@ -9,6 +11,21 @@ interface LoginPageProps {}
 const LoginPage: FC<LoginPageProps> = () => {
   const styles = useStyles();
   const dispatch = useAppDispatch();
+
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on('connected', () => console.log('Connected'));
+    socket.on('onMessage', (payload: MessageEventPayload) => {
+      console.log('Message Received');
+      const { conversation, ...message } = payload;
+      /*   dispatch(addMessage(payload)); */
+    });
+    return () => {
+      socket.off('connected');
+      socket.off('onMessage');
+    };
+  }, []);
 
   return (
     <Page display="flex">
--- src/utils/context/SocketContext.tsx
+++ src/utils/context/SocketContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
 export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!, {
-  reconnectionAttempts: 3,
-  withCredentials: true,
+  reconnectionAttempts: 33,
+  withCredentials: false,
 });
 export const SocketContext = createContext(socket);
