1d2e78287781f8b7e51e05bf844efde5b36b4e44
VitaliiPolushkinDigis <91121101+VitaliiPolushkinDigis@users.noreply.github.com>
2023-09-17 18:22:13 +0300

    create custom utility types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,9 @@
+import { Sex } from './workers/longProcesses/enums';
+
 export type ConversationType = {
   id: number;
-  creator: User;
-  recipient: User;
+  creator: UserWithoutPassword;
+  recipient: UserWithoutPassword;
   createdAt: Date | string;
 };
 export type CreateConversationParams = {
@@ -14,22 +16,37 @@ export type ConversationMessage = {
   messages: MessageType[];
 };
 
-export type CreateUserParams = {
+/* export type CreateUserParams = {
   email: string;
   firstName: string;
   lastName: string;
   password: string;
-};
+}; */
 export type UserCredentialsParams = {
   email: string;
   password: string;
 };
+
 export type User = {
   id: number;
   email: string;
   firstName: string;
   lastName: string;
+  password: string;
+  profileId: number;
+  sex?: Sex;
 };
+
+type ExcludeField<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+export type UserWithoutPassword = ExcludeField<User, 'password'>;
+
+type OmitFields<T, K extends keyof T> = Required<Pick<T, Exclude<keyof T, K>>> &
+  Partial<Pick<T, K>>;
+
+export type CreateUserParams = OmitFields<User, 'id' | 'profileId'>;
 
 export type MessageType = {
   id: number;
